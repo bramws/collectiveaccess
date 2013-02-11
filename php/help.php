d0a1547601b8c26609cbf2c7e1648b1b59e61c5a
bram <b@bram.ws>
2013-02-11 13:24:29 +0100

    jj

--- php/help.php
+++ php/help.php
@@ -1,9 +1,9 @@
 <?php
-$this->default['host']     = getenv("OPENSHIFT_DB_HOST");
-$this->default['port']     = getenv("OPENSHIFT_DB_PORT");
-$this->default['login']    = getenv("OPENSHIFT_DB_USERNAME");
-$this->default['password'] = getenv("OPENSHIFT_DB_PASSWORD");
-$this->default['database'] = getenv("OPENSHIFT_APP_NAME")
+$this->default['host']     = getenv("$OPENSHIFT_DB_HOST");
+$this->default['port']     = getenv("$OPENSHIFT_DB_PORT");
+$this->default['login']    = getenv("$OPENSHIFT_DB_USERNAME");
+$this->default['password'] = getenv("$OPENSHIFT_DB_PASSWORD");
+$this->default['database'] = getenv("$OPENSHIFT_APP_NAME")
 Echo "Hello World";
 echo "$host";
 
